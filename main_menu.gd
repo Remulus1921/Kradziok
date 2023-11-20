17c713c57ca7ed298686d5c1ee233f6f7232a2c1
Arek2017 <agront@interia.pl>
2023-11-20 16:22:56 +0100

    Created main menu and settings (unfinished)

---
+++ main_menu.gd
@@ -0,0 +1,46 @@
+extends Control
+
+@onready var menu := $MarginContainer/Menu
+@onready var settings := $Settings
+@onready var musicSlider := $Settings/MarginContainer/VBoxContainer/MusicSlider
+@onready var soundSlider := $Settings/MarginContainer/VBoxContainer/SoundSlider
+
+func _ready():
+	volume(1, linear_to_db(2));
+
+func _on_new_game_button_pressed():
+	get_tree().change_scene_to_file("res://test.tscn")
+
+func _on_settings_button_pressed():
+	menu.visible = false;
+	settings.visible = true;
+
+func _on_return_button_pressed():
+	settings.visible = false;
+	menu.visible = true;
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+func _on_music_enable_toggled(button_pressed):
+	if button_pressed == true:
+		musicSlider.editable = true;
+	else:
+		musicSlider.editable = false;
+		musicSlider.value = 0;
+
+func _on_sound_enable_toggled(button_pressed):
+	if button_pressed == true:
+		soundSlider.editable = true;
+	else:
+		soundSlider.editable = false;
+		soundSlider.value = 0;
+
+func volume(bus_index, value):
+	AudioServer.set_bus_volume_db(bus_index, value);
+
+func _on_music_slider_value_changed(value):
+	volume(1, linear_to_db(value));	
+
+func _on_sound_slider_value_changed(value):
+	volume(2, value);
