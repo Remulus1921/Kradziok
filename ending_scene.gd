df67ec1854205992888e8bada3e09300016f1df9
Arek2017 <agront@interia.pl>
2023-11-24 10:24:41 +0100

    Created menu in game, few things finished and improved

---
+++ ending_scene.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_next_game_button_pressed():
+	pass # Replace with function body.
+
+func _on_quit_to_the_menu_button_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
